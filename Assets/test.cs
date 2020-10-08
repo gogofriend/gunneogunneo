db995503f35cedb6c3835eba0d65d1c5666a4466
minjuu <minju9809@naver.com>
2020-10-09 04:29:57 +0900

    stage1 차 도로 -1

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,12 +7,20 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            transform.position += new Vector3(0, 0, 0.1f);
+        }
     }
-}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Obstacle"))
+            Debug.Log("충돌");
+    }
+}//충돌 잘되는지 테스트 해봐써.._민주
