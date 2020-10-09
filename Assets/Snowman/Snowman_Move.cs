7ddb498cc13eb064f2ef5b7ba219b7a119cc31d1
toshiny6 <67131839+toshiny6@users.noreply.github.com>
2020-10-09 15:47:09 +0900

    move+reverse
    아이템,움직이기

---
+++ Assets/Snowman/Snowman_Move.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowman_Move : MonoBehaviour
+{
+    float startTime;
+    float finishTime;
+    bool reverse;
+
+    Vector3 pos;
+    float step = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        reverse = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reverse == false)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                pos += transform.forward * step;
+
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                pos += transform.forward * step;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                pos += transform.forward * step;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                pos += transform.forward * step;
+            }
+        }
+        if (reverse)
+        {
+            startTime += Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                pos += transform.forward * step;
+
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                pos += transform.forward * step;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                pos += transform.forward * step;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                pos += transform.forward * step;
+            }
+            if (startTime >= finishTime)
+            {
+                reverse = false;
+            }
+        }
+
+        transform.position = pos;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("T");
+        if (other.gameObject.name == "reverse")
+        {
+            startTime = Time.time;
+            finishTime = startTime + 3f;
+            reverse = true;
+
+            Destroy(other.gameObject, 0);
+
+        }
+    }
+
+}
