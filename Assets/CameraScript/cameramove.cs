1b45cabad080d86b83a4e1d5be9f83e825910d1e
wonyoung2323 <isabella23@naver.com>
2020-10-11 17:52:05 +0900

    카메라
    카메라

--- Assets/CameraScript/cameramove.cs
+++ Assets/CameraScript/cameramove.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class cameramove : MonoBehaviour
 {
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 8f;
+    public float offsetZ = -9f;
+
+    Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        //gameObject.transform.position += new Vector3(0.1f, 0.1f, 0.1f);
+        pos.x = player.transform.position.x + offsetX;
+        pos.y = player.transform.position.y + offsetY;
+        pos.z = player.transform.position.z + offsetZ;
+
+        transform.position = pos;
     }
 }
