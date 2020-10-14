335c9f0911426dfeb1c2fee29ddffe8f70f2b24c
minjuu <minju9809@naver.com>
2020-10-15 00:52:14 +0900

    1

--- Assets/SceneScript/inputranking.cs
+++ Assets/SceneScript/inputranking.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class inputranking : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public InputField inputField;
+    public static string fieldText;
+    public void onStart()
     {
-        
+        SceneManager.LoadScene("Ranking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
